1657d3faa323847a31e112b4c8e433b6363ff178
riccardo <riccardoberbeglia03@gmail.com>
2022-02-04 11:04:40 +0100

    Custom scrollable overlay text.

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 
 interface OverlayProps {
   scroll: React.MutableRefObject<number>;
-  //setScroll: React.Dispatch<React.SetStateAction<number>>;
 }
 
 const Overlay = ({ scroll }: OverlayProps) => {
@@ -17,8 +16,36 @@ const Overlay = ({ scroll }: OverlayProps) => {
 
   return (
     <div id="info" onScroll={scrollHandler}>
-      <div id="scroller"></div>
-      <h1 style={{ color: "white" }}>{scrollValue}</h1>
+      <div style={{ height: "400vh" }}>
+        <div className="dot">
+          <h1>start</h1>
+          the animation starts here
+        </div>
+      </div>
+      <div style={{ height: "400vh" }}>
+        <div className="dot">
+          <h1>spheres</h1>
+          these are spheres
+        </div>
+      </div>
+      <div style={{ height: "400vh" }}>
+        <div className="dot">
+          <h1>cone</h1>
+          this is a cone
+        </div>
+      </div>
+      <div style={{ height: "400vh" }}>
+        <div className="dot">
+          <h1>other spheres</h1>
+          pink spheres
+        </div>
+      </div>
+      <div style={{ height: "200vh" }}>
+        <div className="dot">
+          <h1>end</h1>
+          the animation ends here
+        </div>
+      </div>
     </div>
   );
 };
